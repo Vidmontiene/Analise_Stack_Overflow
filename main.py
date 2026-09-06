5d9484c045a9ebcef6b7b688ba766b1ec48e9b68
Camila Alves Vidmontiene <camilavidmontiene@gmail.com>
2026-09-06 15:34:12 -0300

    Imagens top 10

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sqlite3
 conexao = sqlite3.connect("bancos/pesquisa.db")
 cursor = conexao.cursor()
 
+# SQL do top 10
 def top_10_ano(ano):
 
   cursor.execute(
@@ -24,8 +25,27 @@ def top_10_ano(ano):
 
   return cursor.fetchall()
 
-anos = [str(x) for x in range(2011, 2026)]
+# Pega as top 10 linguagens de cada ano
+def pegar_top_10_por_ano():
+  anos = [str(x) for x in range(2011, 2026)]
 
-for ano in anos:
-  resultado = top_10_ano(ano)
-  print(resultado)
+  for ano in anos:
+    resultado = top_10_ano(ano)
+
+    linguagens = [linha[1] for linha in resultado]
+    quantidades = [linha[2] for linha in resultado]
+    plt.figure(figsize=(12, 6))
+
+    plt.bar(linguagens, quantidades)
+
+    plt.title(f'Top 10 linguagens mais usadas em {ano}')
+    plt.xlabel('Linguagem')
+    plt.ylabel('Quantidade')
+
+    plt.xticks(rotation=45, ha='right')
+
+    plt.tight_layout()
+    plt.savefig(f'graficos/top10_por_ano/top10_{ano}.png', dpi=300)
+
+conexao.close()
+    
